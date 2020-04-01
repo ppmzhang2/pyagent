e14db40acd0200c3ce8aa400cfddf4be06198ab7
Zhang <ztz2000@gmail.com>
2020-04-02 04:31:31 +0800

    [new] main function

---
+++ wsproxy/__main__.py
@@ -0,0 +1,41 @@
+import sys
+from typing import Optional
+
+import wsproxy.client_server as client
+import wsproxy.proxy_server as server
+
+args = sys.argv[1:]
+
+funcs = {'server': (0, server.run), 'client': (0, client.run)}
+
+
+def request() -> Optional[str]:
+    """get the input request string, which will be then mapped as key to get
+    main method
+
+    :return:
+    """
+    try:
+        return str(args[0])
+    except IndexError:
+        return None
+
+
+def main() -> None:
+    if request() is None:
+        raise TypeError('expect at least one input')
+
+    n_args, func = funcs.get(request(), (None, None))
+
+    if func is None:
+        raise TypeError('input request is not valid, accept only {}'.format(
+            list(funcs.keys())))
+
+    if n_args == 0:
+        func()
+    else:
+        raise TypeError('input error')
+
+
+if __name__ == '__main__':
+    main()
