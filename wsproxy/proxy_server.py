4ce1bff2d3db2ea5cc708f194e8e58cd818736aa
Zhang <ztz2000@gmail.com>
2020-04-04 04:00:12 +0800

    [refactor] methods re-org as 'handshake' and other proxy server protocol methods

--- wsproxy/proxy_server.py
+++ wsproxy/proxy_server.py
@@ -7,93 +7,18 @@ from struct import pack, unpack
 from typing import Optional, NoReturn, Tuple
 
 import wsproxy.config as cfg
-from wsproxy.base_protocol import BaseTcpProtocol, AesTcpProtocol, dec
+from wsproxy.base_protocol import BaseTcpProtocol, AesTcpProtocol
 
 logging.config.dictConfig(cfg.logging)
 logger = logging.getLogger(__name__)
 
 
 class RemoteTcpProtocol(BaseTcpProtocol):
-    _INIT, _CONN, _DATA = 0, 1, 2
-
-    def __init__(self):
+    def __init__(self, reader: asyncio.StreamReader,
+                 writer: asyncio.StreamWriter):
         super().__init__()
-        self.reader = None
-        self.writer = None
-        self._state: int = self._INIT
-
-    @property
-    def init_phase(self):
-        return self._state == self._INIT
-
-    @property
-    def conn_phase(self):
-        return self._state == self._CONN
-
-    @property
-    def data_phase(self):
-        return self._state == self._DATA
-
-    async def change_state(self, local: ProxyServerProtocol) -> None:
-        if not local.initiated:
-            return
-        data = await local.recv()
-        if data is None:
-            return
-        if self.init_phase:
-            assert data[0] == 0x05
-            # no auth
-            await local.send(pack('!BB', 0x05, 0x00))
-            self._state = self._CONN
-            return
-        elif self.conn_phase:
-            ver, cmd, rsv, atype = data[:4]
-            assert ver == 0x05 and cmd == 0x01
-
-            if atype == 3:  # domain
-                length = data[4]
-                host, nxt = data[5:5 + length], 5 + length
-            elif atype == 1:  # ipv4
-                host, nxt = socket.inet_ntop(socket.AF_INET, data[4:8]), 8
-            elif atype == 4:  # ipv6
-                host, nxt = socket.inet_ntop(socket.AF_INET6, data[4:20]), 20
-            else:
-                raise ValueError("type specified not supported")
-            port = unpack('!H', data[nxt:nxt + 2])[0]
-            reader, writer = await asyncio.open_connection(host=host,
-                                                           port=port)
-            proxy_hostname = unpack(
-                "!I", socket.inet_aton(cfg.proxy_server['host_public']))[0]
-
-            self.reader = reader
-            self.writer = writer
-            self._state = self._DATA
-            await local.send(
-                pack('!BBBBIH', 0x05, 0x00, 0x00, 0x01, proxy_hostname,
-                     cfg.proxy_server['port']))
-            return
-        else:
-            return
-
-    @dec
-    async def remote_to_local(self, local: ProxyServerProtocol) -> NoReturn:
-        if self.data_phase:
-            while True:
-                data = await self.recv()
-                if data is None:
-                    break
-                await local.send(data)
-        else:
-            pass
-
-    @dec
-    async def local_to_remote(self, local: ProxyServerProtocol) -> NoReturn:
-        if self.data_phase:
-            while True:
-                data = await local.recv()
-                if data is None:
-                    break
-                await self.send(data)
+        self.reader = reader
+        self.writer = writer
 
 
 class ProxyServerProtocol(AesTcpProtocol):
@@ -119,20 +44,87 @@ class ProxyServerProtocol(AesTcpProtocol):
         """
         return self.writer.get_extra_info('sockname')
 
-    async def exchange_data(self):
-        remote = RemoteTcpProtocol()
-        while not remote.data_phase:
-            await remote.change_state(self)
-        # Pipe the streams, execution order is uncertain
-        # can also use 'await asyncio.gather'
-        asyncio.ensure_future(remote.local_to_remote(self))
-        asyncio.ensure_future(remote.remote_to_local(self))
+    async def handshake(self) -> Optional[RemoteTcpProtocol]:
+        """apply immediately after connection established with client
+
+        :return: remote TCP protocol is successful, None otherwise
+        """
+        init_req = await self.recv()
+        if not init_req:
+            logger.info('handshake failed: no data received')
+            return
+        elif not init_req[0] == 0x05:
+            logger.info('handshake failed: not SOCKS5')
+            return
+        else:
+            pass
+
+        await self.send(pack('!BB', 0x05, 0x00))
+        logger.info(f'try to accept {self.peer} with no auth...')
+
+        conn_req = await self.recv()
+        ver, cmd, rsv, atype = conn_req[:4]
+        assert ver == 0x05 and cmd == 0x01
+
+        if atype == 3:  # domain
+            url_len = conn_req[4]
+            host, port_idx = conn_req[5:5 + url_len], 5 + url_len
+        elif atype == 1:  # ipv4
+            host, port_idx = socket.inet_ntop(socket.AF_INET, conn_req[4:8]), 8
+        elif atype == 4:  # ipv6
+            host, port_idx = socket.inet_ntop(socket.AF_INET6,
+                                              conn_req[4:20]), 20
+        else:
+            logger.error(f'handshake failed: AType {atype} not supported')
+            return
+
+        port = unpack('!H', conn_req[port_idx:port_idx + 2])[0]
+
+        try:
+            reader, writer = await asyncio.open_connection(host=host,
+                                                           port=port)
+        except ConnectionRefusedError as e:
+            logger.error(f'handshake failed: {e}')
+            return
+        proxy_hostname = unpack(
+            "!I", socket.inet_aton(cfg.proxy_server['host_public']))[0]
+        respond = pack('!BBBBIH', 0x05, 0x00, 0x00, 0x01, proxy_hostname,
+                       cfg.proxy_server['port'])
+        await self.send(respond)
+        logger.info(f'handshake successful with {self.peer}')
+        return RemoteTcpProtocol(reader, writer)
+
+    async def from_remote(self, remote: RemoteTcpProtocol) -> NoReturn:
+        while not self.closed:
+            data = await remote.recv()
+            if data is None:
+                break
+            await self.send(data)
+
+    async def to_remote(self, remote: RemoteTcpProtocol) -> NoReturn:
+        while not self.closed:
+            data = await self.recv()
+            if data is None:
+                break
+            await remote.send(data)
+
+    async def exchange_data(self) -> None:
+        remote = await self.handshake()
+        if not remote:
+            await self.close()
+            return
+        else:
+            # Pipe the streams, execution order is uncertain
+            # can also use 'await asyncio.gather'
+            asyncio.ensure_future(self.from_remote(remote))
+            asyncio.ensure_future(self.to_remote(remote))
+            return
 
 
 def run():
     def handle_client(reader, writer):
         local = ProxyServerProtocol(reader, writer)
-        # logger.debug(f'initiated from: {local.peer}')
+        logger.info(f'new client from: {local.peer}')
         return asyncio.ensure_future(local.exchange_data())
 
     async def service(h: str = cfg.proxy_server['host'],
