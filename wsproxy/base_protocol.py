58ff49b0ae33967cf9337216cb2cd8e0490931fd
Zhang <ztz2000@gmail.com>
2020-04-04 07:52:48 +0800

    [refactor] SOCKS5 handshake and other methods moved to base class

--- wsproxy/base_protocol.py
+++ wsproxy/base_protocol.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 import asyncio
 import logging.config
 from functools import wraps
-from typing import Optional, NoReturn
+import socket
+from struct import pack, unpack
+from typing import Optional, NoReturn, Tuple
 
 import wsproxy.config as cfg
 from wsproxy.enigma import AesGcm
@@ -66,10 +68,79 @@ class BaseTcpProtocol(object):
                 pass
 
     @property
+    def peer(self) -> Optional[Tuple[str, int]]:
+        """address & port of a peer, from which initiates connection
+
+        :return: a tuple of 1. address; 2. port
+        """
+        return self.writer.get_extra_info('peername')
+
+    @property
+    def sock(self) -> Optional[Tuple[str, int]]:
+        """socket binding address & port
+
+        :return: a tuple of 1. address; 2. port
+        """
+        return self.writer.get_extra_info('sockname')
+
+    @property
     def closed(self) -> Optional[bool]:
         if not self.initiated:
             return None
         return self.writer.is_closing()
+
+    async def handshake_socks5(
+        self
+    ) -> Tuple[Optional[asyncio.StreamReader], Optional[asyncio.StreamWriter]]:
+        """handshake handler for socks5 protocol
+
+        :return: a tuple of stream reader and writer if handshake successful,
+          otherwise Tuple[None, None]
+        """
+        init_req = await self.recv()
+        if not init_req:
+            logger.info('handshake failed: no data received')
+            return
+        elif not init_req[0] == 0x05:
+            logger.info('handshake failed: not SOCKS5')
+            return
+        else:
+            pass
+
+        await self.send(pack('!BB', 0x05, 0x00))
+        logger.info(f'try to accept {self.peer} with no auth...')
+
+        conn_req = await self.recv()
+        ver, cmd, rsv, atype = conn_req[:4]
+        assert ver == 0x05 and cmd == 0x01
+
+        if atype == 3:  # domain
+            url_len = conn_req[4]
+            host, port_idx = conn_req[5:5 + url_len], 5 + url_len
+        elif atype == 1:  # ipv4
+            host, port_idx = socket.inet_ntop(socket.AF_INET, conn_req[4:8]), 8
+        elif atype == 4:  # ipv6
+            host, port_idx = socket.inet_ntop(socket.AF_INET6,
+                                              conn_req[4:20]), 20
+        else:
+            logger.error(f'handshake failed: AType {atype} not supported')
+            return
+
+        port = unpack('!H', conn_req[port_idx:port_idx + 2])[0]
+
+        try:
+            reader, writer = await asyncio.open_connection(host=host,
+                                                           port=port)
+        except ConnectionRefusedError as e:
+            logger.error(f'handshake failed: {e}')
+            return
+        proxy_hostname = unpack(
+            "!I", socket.inet_aton(cfg.proxy_server['host_public']))[0]
+        respond = pack('!BBBBIH', 0x05, 0x00, 0x00, 0x01, proxy_hostname,
+                       cfg.proxy_server['port'])
+        await self.send(respond)
+        logger.info(f'handshake successful with {self.peer}')
+        return reader, writer
 
 
 class AesTcpProtocol(BaseTcpProtocol):
--- wsproxy/proxy_server.py
+++ wsproxy/proxy_server.py
@@ -2,9 +2,7 @@ from __future__ import annotations
 
 import asyncio
 import logging.config
-import socket
-from struct import pack, unpack
-from typing import Optional, NoReturn, Tuple
+from typing import Optional, NoReturn
 
 import wsproxy.config as cfg
 from wsproxy.base_protocol import BaseTcpProtocol, AesTcpProtocol
@@ -30,71 +28,16 @@ class ProxyServerProtocol(AesTcpProtocol):
         self.reader = reader
         self.writer = writer
 
-    @property
-    def peer(self) -> Optional[Tuple[str, int]]:
-        """address & port of a peer, from which initiates connection
-
-        :return: a tuple of 1. address; 2. port
-        """
-        return self.writer.get_extra_info('peername')
-
-    @property
-    def sock(self) -> Optional[Tuple[str, int]]:
-        """socket binding address & port
-
-        :return: a tuple of 1. address; 2. port
-        """
-        return self.writer.get_extra_info('sockname')
-
     async def handshake(self) -> Optional[RemoteTcpProtocol]:
         """apply immediately after connection established with client
 
         :return: remote TCP protocol is successful, None otherwise
         """
-        init_req = await self.recv()
-        if not init_req:
-            logger.info('handshake failed: no data received')
-            return
-        elif not init_req[0] == 0x05:
-            logger.info('handshake failed: not SOCKS5')
-            return
+        reader, writer = await self.handshake_socks5()
+        if reader is None or writer is None:
+            return None
         else:
-            pass
-
-        await self.send(pack('!BB', 0x05, 0x00))
-        logger.info(f'try to accept {self.peer} with no auth...')
-
-        conn_req = await self.recv()
-        ver, cmd, rsv, atype = conn_req[:4]
-        assert ver == 0x05 and cmd == 0x01
-
-        if atype == 3:  # domain
-            url_len = conn_req[4]
-            host, port_idx = conn_req[5:5 + url_len], 5 + url_len
-        elif atype == 1:  # ipv4
-            host, port_idx = socket.inet_ntop(socket.AF_INET, conn_req[4:8]), 8
-        elif atype == 4:  # ipv6
-            host, port_idx = socket.inet_ntop(socket.AF_INET6,
-                                              conn_req[4:20]), 20
-        else:
-            logger.error(f'handshake failed: AType {atype} not supported')
-            return
-
-        port = unpack('!H', conn_req[port_idx:port_idx + 2])[0]
-
-        try:
-            reader, writer = await asyncio.open_connection(host=host,
-                                                           port=port)
-        except ConnectionRefusedError as e:
-            logger.error(f'handshake failed: {e}')
-            return
-        proxy_hostname = unpack(
-            "!I", socket.inet_aton(cfg.proxy_server['host_public']))[0]
-        respond = pack('!BBBBIH', 0x05, 0x00, 0x00, 0x01, proxy_hostname,
-                       cfg.proxy_server['port'])
-        await self.send(respond)
-        logger.info(f'handshake successful with {self.peer}')
-        return RemoteTcpProtocol(reader, writer)
+            return RemoteTcpProtocol(reader, writer)
 
     async def from_remote(self, remote: RemoteTcpProtocol) -> NoReturn:
         while not self.closed:
